29aa6e718592b951d4996eabb9f41cef785fa850
dpcks0509 <dpcks0509@gmail.com>
2026-02-02 10:43:17 +0900

    feat: pinnedCafeteria -> selectedCafeteria로 수정

--- app/src/main/java/pusan/university/plato_calendar/presentation/cafeteria/CafeteriaViewModel.kt
+++ app/src/main/java/pusan/university/plato_calendar/presentation/cafeteria/CafeteriaViewModel.kt
@@ -9,8 +9,7 @@ import pusan.university.plato_calendar.data.local.database.CafeteriaDataStore
 import pusan.university.plato_calendar.domain.entity.Cafeteria
 import pusan.university.plato_calendar.domain.repository.CafeteriaRepository
 import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaEvent
-import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaEvent.PinCafeteria
-import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaEvent.UnPinCafeteria
+import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaEvent.SelectCafeteria
 import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaSideEffect
 import pusan.university.plato_calendar.presentation.cafeteria.intent.CafeteriaState
 import pusan.university.plato_calendar.presentation.common.base.BaseViewModel
@@ -20,60 +19,59 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CafeteriaViewModel
-@Inject
-constructor(
-    private val loadingManager: LoadingManager,
-    private val cafeteriaRepository: CafeteriaRepository,
-    private val cafeteriaDataStore: CafeteriaDataStore,
-) : BaseViewModel<CafeteriaState, CafeteriaEvent, CafeteriaSideEffect>(initialState = CafeteriaState()) {
-    override suspend fun handleEvent(event: CafeteriaEvent) {
-        when (event) {
-            is PinCafeteria -> cafeteriaDataStore.setPinnedCafeteria(event.cafeteria)
-            UnPinCafeteria -> cafeteriaDataStore.clearPinnedCafeteria()
+    @Inject
+    constructor(
+        private val loadingManager: LoadingManager,
+        private val cafeteriaRepository: CafeteriaRepository,
+        private val cafeteriaDataStore: CafeteriaDataStore,
+    ) : BaseViewModel<CafeteriaState, CafeteriaEvent, CafeteriaSideEffect>(initialState = CafeteriaState()) {
+        override suspend fun handleEvent(event: CafeteriaEvent) {
+            when (event) {
+                is SelectCafeteria -> cafeteriaDataStore.setSelectedCafeteria(event.cafeteria)
+            }
         }
-    }
 
-    init {
-        observePinnedCafeteria()
-        getCafeteriaWeeklyPlan()
-    }
+        init {
+            observeSelectedCafeteria()
+            getCafeteriaWeeklyPlan()
+        }
 
-    private fun observePinnedCafeteria() {
-        viewModelScope.launch {
-            cafeteriaDataStore.pinnedCafeteria.collect { pinnedCafeteria ->
-                setState { copy(pinnedCafeteria = pinnedCafeteria) }
+        private fun observeSelectedCafeteria() {
+            viewModelScope.launch {
+                cafeteriaDataStore.selectedCafeteria.collect { selectedCafeteria ->
+                    setState { copy(selectedCafeteria = selectedCafeteria) }
+                }
+            }
+        }
+
+        private fun getCafeteriaWeeklyPlan() {
+            viewModelScope.launch {
+                loadingManager.updateLoading(true)
+
+                val results =
+                    Cafeteria.entries
+                        .map { cafeteria ->
+                            async {
+                                cafeteriaRepository.getCafeteriaWeeklyPlan(cafeteria = cafeteria)
+                            }
+                        }.awaitAll()
+
+                var isErrorNotified = false
+
+                val cafeteriaWeeklyPlans =
+                    results
+                        .mapNotNull { result ->
+                            result
+                                .onFailure { throwable ->
+                                    if (!isErrorNotified) {
+                                        ToastEventBus.sendError(throwable.message)
+                                        isErrorNotified = true
+                                    }
+                                }.getOrNull()
+                        }
+
+                setState { copy(cafeteriaWeeklyPlans = cafeteriaWeeklyPlans) }
+                loadingManager.updateLoading(false)
             }
         }
     }
-
-    private fun getCafeteriaWeeklyPlan() {
-        viewModelScope.launch {
-            loadingManager.updateLoading(true)
-
-            val results =
-                Cafeteria.entries
-                    .map { cafeteria ->
-                        async {
-                            cafeteriaRepository.getCafeteriaWeeklyPlan(cafeteria = cafeteria)
-                        }
-                    }.awaitAll()
-
-            var isErrorNotified = false
-
-            val cafeteriaWeeklyPlans =
-                results
-                    .mapNotNull { result ->
-                        result
-                            .onFailure { throwable ->
-                                if (!isErrorNotified) {
-                                    ToastEventBus.sendError(throwable.message)
-                                    isErrorNotified = true
-                                }
-                            }.getOrNull()
-                    }
-
-            setState { copy(cafeteriaWeeklyPlans = cafeteriaWeeklyPlans) }
-            loadingManager.updateLoading(false)
-        }
-    }
-}
--- app/src/main/java/pusan/university/plato_calendar/presentation/cafeteria/intent/CafeteriaEvent.kt
+++ app/src/main/java/pusan/university/plato_calendar/presentation/cafeteria/intent/CafeteriaEvent.kt
@@ -4,6 +4,7 @@ import pusan.university.plato_calendar.domain.entity.Cafeteria
 import pusan.university.plato_calendar.presentation.common.base.UiEvent
 
 sealed interface CafeteriaEvent : UiEvent {
-    data class PinCafeteria(val cafeteria: Cafeteria): CafeteriaEvent
-    object UnPinCafeteria: CafeteriaEvent
+    data class SelectCafeteria(
+        val cafeteria: Cafeteria,
+    ) : CafeteriaEvent
 }
