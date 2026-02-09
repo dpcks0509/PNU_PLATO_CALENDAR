450dd63972d2a267f8a2381a1796b316796c1b5a
dpcks0509 <dpcks0509@gmail.com>
2026-02-09 22:11:53 +0900

    feat: type 색상 수정

--- app/src/main/java/pusan/university/plato_calendar/presentation/todo/component/ToDoScheduleItem.kt
+++ app/src/main/java/pusan/university/plato_calendar/presentation/todo/component/ToDoScheduleItem.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -103,7 +104,7 @@ fun ToDoScheduleItem(
                     Text(
                         text = type,
                         fontSize = 12.sp,
-                        color = White,
+                        color = Color.White,
                         fontWeight = FontWeight.SemiBold,
                     )
                 }
