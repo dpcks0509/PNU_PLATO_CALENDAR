e4a4d6999621929f7e25489492fb4bb6a3de9d5a
dpcks0509 <dpcks0509@gmail.com>
2026-03-07 16:02:50 +0900

    refactor: 에러 로깅 제거

--- app/src/main/java/pusan/university/plato_calendar/data/util/ApiResponse.kt
+++ app/src/main/java/pusan/university/plato_calendar/data/util/ApiResponse.kt
@@ -1,6 +1,5 @@
 package pusan.university.plato_calendar.data.util
 
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import retrofit2.Response
 import java.io.IOException
 import kotlin.coroutines.cancellation.CancellationException
@@ -55,16 +54,6 @@ suspend inline fun <T> handleApiResponse(crossinline call: suspend () -> Respons
     } catch (e: IOException) {
         ApiResponse.Failure.NetworkException(exception = e, message = "네트워크 연결을 확인해주세요.")
     } catch (e: Exception) {
-        // 에러 원인 파악 후 제거
-        FirebaseCrashlytics.getInstance().apply {
-            log("UnknownException in handleApiResponse")
-            log("exception: ${e.javaClass.name}: ${e.localizedMessage}")
-            e.cause?.let { cause ->
-                log("caused by: ${cause.javaClass.name}: ${cause.localizedMessage}")
-            }
-            recordException(e)
-        }
-
         ApiResponse.Failure.UnknownException(
             exception = e,
             message = "알 수 없는 오류가 발생했습니다.\n잠시후 다시 시도해주세요.",
