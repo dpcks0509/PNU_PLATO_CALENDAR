efb2983b2ee289b8a66b891caff0ce9ac647a59b
dpcks0509 <dpcks0509@gmail.com>
2026-04-01 17:42:15 +0900

    release: v1.2.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "pusan.university.plato_calendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.2.11"
+        versionCode = 31
+        versionName = "1.2.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
