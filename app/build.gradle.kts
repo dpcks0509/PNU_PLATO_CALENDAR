5facfb46c523c23f30c1f64d1a70e510d0aef0a8
dpcks0509 <dpcks0509@gmail.com>
2026-04-11 16:43:53 +0900

    release: v1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "pusan.university.plato_calendar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
-        versionName = "1.3.2"
+        versionCode = 35
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
